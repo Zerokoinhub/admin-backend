64d1a6ee65fdeff3f6be330e207cce9f73e6e96e
Muhammad Waleed <153022789+WaleedFromPakistan@users.noreply.github.com>
2025-06-28 00:24:03 +0500

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const allowedOrigins = ['https://admin.zerokoin.com', 'http://localhost:3000','https://admin-frontend-jet-eta.vercel.app/'];
+const allowedOrigins = ['https://admin-frontend-jet-eta.vercel.app/', 'http://localhost:3000'];
 
 app.use(cors({
   origin: function (origin, callback) {
